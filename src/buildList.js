a90e3cd516df1bb178cd0e0dfdfa88b9a1a137c7
DefiProj <defiproj@protonmail.com>
2021-07-22 13:08:15 -0600

    feat(tokenlist): Updated tokenlist

--- src/buildList.js
+++ src/buildList.js
@@ -3,9 +3,9 @@ const { version } = require("../package.json");
 const ropsten = require("./tokens/ropsten.json");
 const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");*/
+const kovan = require("./tokens/kovan.json");
 const bscMainnet = require("./tokens/bsc-mainnet.json");
-const bscTestnet = require("./tokens/bsc-testnet.json");
+const bscTestnet = require("./tokens/bsc-testnet.json");*/
 const harmonyMainnet = require("./tokens/harmony-mainnet.json");
 const harmonyTestnet = require("./tokens/harmony-testnet.json");
 
@@ -22,7 +22,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "https://legacy.lootswap.finance/tokens/LOOT.png",
     keywords: ["lootswap", "default"],
-    tokens: [...bscMainnet, ...bscTestnet, ...harmonyMainnet, ...harmonyTestnet]
+    tokens: [...harmonyMainnet, ...harmonyTestnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
